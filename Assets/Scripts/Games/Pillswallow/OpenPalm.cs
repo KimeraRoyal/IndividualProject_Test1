f684bfb5562dd6eb03bffcfe6454893af56b2dc3
Kimera Royal <royalkimera01@gmail.com>
2024-04-06 17:39:38 +0100

    Swallowing pills is now REAL

--- Assets/Scripts/Games/Pillswallow/OpenPalm.cs
+++ Assets/Scripts/Games/Pillswallow/OpenPalm.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using DG.Tweening;
 using IP1.Interaction;
 using UnityEngine;
 
@@ -6,10 +8,24 @@ namespace IP1
     [RequireComponent(typeof(Mover), typeof(Rotator), typeof(MouseMovement))]
     public class OpenPalm : MonoBehaviour
     {
+        private Microgame m_microgame;
+        
         private Mover m_mover;
         private Rotator m_rotator;
         
         private MouseMovement m_mouseMovement;
+
+        private SpriteAnimationSet[] m_animationSet;
+
+        [SerializeField] private Transform m_pillPrefab;
+        [SerializeField] private Transform m_pillSpawnPoint;
+        [SerializeField] private Vector3 m_pillSpawnMinOffset, m_pillSpawnMaxOffset;
+        [SerializeField] private float m_pillSpawnInterval = 1.0f;
+        
+        [SerializeField] private GameObject m_colliders;
+
+        [SerializeField] private float m_dropZ;
+        [SerializeField] private float m_dropSpriteHoldTime = 1.0f;
 
         [SerializeField] private float m_moveInSpeed = 1.0f;
         [SerializeField] private float m_moveOutSpeed = 1.0f;
@@ -18,15 +34,26 @@ namespace IP1
         
         private void Awake()
         {
+            m_microgame = GetComponentInParent<Microgame>();
+            
             m_mover = GetComponent<Mover>();
             m_rotator = GetComponent<Rotator>();
             
             m_mouseMovement = GetComponent<MouseMovement>();
+
+            m_animationSet = GetComponentsInChildren<SpriteAnimationSet>();
         }
 
         private void Start()
         {
             m_mouseMovement.OnMouseMoved += OnMouseMoved;
+
+            SpawnPills(3);
+        }
+
+        private void Update()
+        {
+            Drop();
         }
 
         private void OnMouseMoved(Vector2 _movement)
@@ -34,6 +61,48 @@ namespace IP1
             var speed = _movement.y > 0 ? m_moveOutSpeed : m_moveInSpeed;
             m_mover.TargetPosition += Vector3.forward * (_movement.y * speed);
             m_rotator.TargetRotation += Vector3.forward * (-_movement.x * m_rotateArmSpeed);
+        }
+
+        public void SpawnPills(int _count)
+        {
+            StartCoroutine(SpawnPillLoop(_count));
+        }
+
+        private IEnumerator SpawnPillLoop(int _count)
+        {
+            for (var i = 0; i < _count; i++)
+            {
+                if(i > 0) { yield return new WaitForSeconds(m_pillSpawnInterval); }
+
+                var position = new Vector3();
+                for (var axis = 0; axis < 3; axis++)
+                {
+                    position[axis] = Random.Range(m_pillSpawnMinOffset[axis], m_pillSpawnMaxOffset[axis]);
+                }
+                Instantiate(m_pillPrefab, m_pillSpawnPoint.position + position, Quaternion.identity, m_pillSpawnPoint);
+            }
+        }
+
+        private void Drop()
+        {
+            if(!m_colliders.activeInHierarchy || transform.localPosition.z > m_dropZ) { return; }
+
+            SetHandAnimation(1);
+            m_colliders.SetActive(false);
+
+            var sequence = DOTween.Sequence();
+            sequence.AppendInterval(m_dropSpriteHoldTime);
+            sequence.AppendCallback(() => { SetHandAnimation(0); });
+            
+            m_microgame.Clear();
+        }
+
+        private void SetHandAnimation(int _index)
+        {
+            foreach (var animationSet in m_animationSet)
+            {
+                animationSet.CurrentAnimationIndex = _index;
+            }
         }
     }
 }
--- Assets/Scripts/Graphics/ColourChanger.cs
+++ Assets/Scripts/Graphics/ColourChanger.cs
@@ -13,8 +13,11 @@ namespace IP1
         
         [SerializeField] private float m_colorChangeTime = 1.0f;
         [SerializeField] private Ease m_colorChangeEase = Ease.Linear;
+
+        [SerializeField] private int m_changeToOnStart = -1;
         
         private Color m_currentColor;
+        private int m_currentColorIndex = -1;
 
         private Tween m_colorChangeTween;
 
@@ -41,13 +44,16 @@ namespace IP1
             if(m_colors.Length < 1) { return; }
             
             CurrentColor = m_colors[0];
+
+            if (m_changeToOnStart >= 0) { ChangeColor(m_changeToOnStart); }
         }
 
         public Tween ChangeColor(int _index)
         {
-            if(m_colors.Length < 1) { return null; }
+            if(_index == m_currentColorIndex || m_colors.Length < 1) { return null; }
             
             _index = Math.Clamp(_index, 0, m_colors.Length);
+            m_currentColorIndex = _index;
 
             if (m_colorChangeTween is { active: true }) { m_colorChangeTween.Kill(); }
 
